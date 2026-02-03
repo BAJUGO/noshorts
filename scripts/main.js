493835d3460a50eb863b5c9e6d647d2c40d999fa
Dima <texetbuy@gmail.com>
2026-02-03 15:15:38 +0300

    First workable version

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
-if (document.location.href.startsWith("https://www.youtube.com/shorts/")) {
-    document.location.href = "https://www.youtube.com";
-    console.log("You have been moved to youtube.com")
+checkUrl()
+let lastUrl = window.location.href;
+
+
+const observer = new MutationObserver(() => {
+    if (window.location.href !== lastUrl) {
+        lastUrl = window.location.href;
+        checkUrl();
+    }
+})
+
+observer.observe(document.querySelector('ytd-app'), {childList: true, subtree: true });
+
+
+function checkUrl() {
+    if (window.location.href.includes("https://www.youtube.com/shorts/")) {
+        location.href = browser.runtime.getURL("../anti-brainrot.html");
+
+    }
 }
