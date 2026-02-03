7702d72cd066f3647e851d8d12c30226b8387698
Dima <texetbuy@gmail.com>
2026-02-03 10:39:46 +0300

    Init version It only works when you reload shorts page

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+if (document.location.href.startsWith("https://www.youtube.com/shorts/")) {
+    document.location.href = "https://www.youtube.com";
+    console.log("You have been moved to youtube.com")
+}
