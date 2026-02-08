94007e9652e2ccd33bb76383cdc1f1ba5fca6686
Dima <texetbuy@gmail.com>
2026-02-08 14:32:13 +0300

    Added x.com as full_prohibited Commit just for commit (maybe will commit later)

--- scripts/full_prohibited.js
+++ scripts/full_prohibited.js
@@ -2,5 +2,5 @@ let cite = JSON.stringify(location.host).replace("www.", "")
 let data = JSON.stringify(Date.now())
 let auth = JSON.stringify("AntiBrainrot")
 
-browser.runtime.sendMessage({cite: cite, data: data, auth:auth})
+browser.runtime.sendMessage({cite: cite, data: data, auth:auth});
 location.href = browser.extension.getURL("../pages/anti_brainrot.html");
